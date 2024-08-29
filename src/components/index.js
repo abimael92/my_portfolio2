b84247800d18064ae398216d9ac1b616dff99514
Abimael <abimael1992g@gmail.com>
2024-08-28 20:45:00 -0600

    component build

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import { Banner } from './Banner';
+import { Contact } from './Contact';
+import { Footer } from './Footer';
+import { MailchimpForm } from './MailchimpForm';
+import { NavBar } from './NavBar';
+import { Newsletter } from './Newsletter';
+import { ProjectCard } from './ProjectCard';
+import { Projects } from './Projects';
+import { Skills } from './Skills';
+
+export {
+    Banner,
+    Contact,
+    Footer,
+    MailchimpForm,
+    NavBar,
+    Newsletter,
+    ProjectCard,
+    Projects,
+    Skills
+};
---
+++ src/theme/theme.js
@@ -0,0 +1,13 @@
+// theme.js
+export const colors = {
+    primary: '#007bff',
+    secondary: '#6c757d',
+    background: '#f8f9fa',
+};
+
+export const breakpoints = {
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+};
